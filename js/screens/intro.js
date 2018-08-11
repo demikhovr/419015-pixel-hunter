362e517534f0eb9bbf845d3601410d2105fa47e1
demikhovr <demikhovr@gmail.com>
2018-08-11 20:45:55 +0300

    Шаблоны из index.html перенесены в соответствующие js-модули

---
+++ js/screens/intro.js
@@ -0,0 +1,11 @@
+import {createElement} from './../util';
+
+const template = `
+  <section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </section>`;
+
+const intro = createElement(template);
+
+export default intro;
